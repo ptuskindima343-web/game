0fdb36d865427ee9980971a921fdaa4f48d4b257
ptuskindima343-web <ptuskindima343@gmail.com>
2026-01-16 18:40:15 +0400

    assets

---
+++ asset/hjthrtjyt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="hjthrt" tilewidth="70" tileheight="70" spacing="2" tilecount="156" columns="12">
+ <image source="tiles_spritesheet_4.png" width="914" height="936"/>
+</tileset>
